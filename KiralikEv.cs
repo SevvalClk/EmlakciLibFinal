6efa7224b57ddf7e7007064e3d57de49d1115bb4
SevvalClk <sevvalcolakoglu27@gmail.com>
2024-06-11 09:23:08 +0300

    Add project files.

---
+++ KiralikEv.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmlakciLibFinal
+{
+    public class KiralikEv : Ev
+    {
+        public int Kira { get; set;}
+        public int Depozito { get; set;}
+        public KiralikEv() { }
+
+        public KiralikEv(int odasayisi, string semt, double alan, int katno, int kira, int depozito):base(odasayisi, katno, semt, alan)
+        {
+            this.Kira = kira;
+            this.Depozito = depozito;
+        }
+
+        public override string EvBilgileri()
+        {
+            return $"{base.EvBilgileri()}\n Kira Fiyatı:{this.Kira}\n Depozito:{this.Depozito}";
+        }
+    }
+}
